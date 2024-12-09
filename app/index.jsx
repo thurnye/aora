6ca30e39866330646485e79f520130142ab4ba15
dan_tonye <danthurnye@gmail.com>
2024-12-08 23:42:05 -0500

    created screens and layouts

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl'>Aora!</Text>
+      <Text className='text-3xl font-black'>Aora!</Text>
       <Link href='/profile'>Go to Profile</Link>
     </View>
   );
